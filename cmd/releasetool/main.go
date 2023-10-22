bf2cf54fca6d3583c57c5316d8da660f0ed25540
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2023-10-23 00:02:25 +0200

    handle release assets easily

---
+++ cmd/releasetool/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	_, lastDir := filepath.Split(wd)
+
+	if lastDir != "out" {
+		fmt.Println("current directory is not out")
+		os.Exit(1)
+	}
+
+	entries := crawlFolder(wd)
+
+	for _, ent := range entries {
+		ext := filepath.Ext(ent)
+		_, fileName := filepath.Split(ent)
+
+		withoutExt := fileName[:len(fileName)-len(ext)]
+
+		_, parentFolderName := filepath.Split(filepath.Dir(ent))
+
+		if parentFolderName == "out" {
+			continue
+		}
+
+		newName := fmt.Sprintf("%s-%s%s", withoutExt, parentFolderName, ext)
+
+		src, err := os.Open(ent)
+
+		if err != nil {
+			panic(err)
+		}
+
+		dstLoc := filepath.Join(wd, newName)
+
+		dst, err := os.Create(dstLoc)
+
+		if err != nil {
+			panic(err)
+		}
+
+		io.Copy(dst, src)
+
+		src.Close()
+		dst.Close()
+
+		err = os.Chmod(dstLoc, 0o755)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func crawlFolder(path string) []string {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	found := []string{}
+
+	for _, ent := range entries {
+		if ent.IsDir() {
+			sub := crawlFolder(filepath.Join(path, ent.Name()))
+			found = append(found, sub...)
+		} else {
+			found = append(found, filepath.Join(path, ent.Name()))
+		}
+
+	}
+
+	return found
+}
